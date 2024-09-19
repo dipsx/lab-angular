3db508d3244dbe4802ccd283799a891f094b7d56
dipsx <104759948+dipsx@users.noreply.github.com>
2024-09-19 18:06:07 -0400

    use api from shared state

--- projectmgt/dashboard/src/app/dashboard/dashboard.component.ts
+++ projectmgt/dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,32 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { sharedStateService } from '@projectmgt/sharedstate';
+import { apiServiceInstance } from '@projectmgt/sharedstate';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent implements OnInit {
   projects: any[] = [];
   tasks: any[] = [];
-  teams: any[] = [];
+  team: any[] = [];
 
   constructor() {}
 
-  sharedState: any;
-  updateSharedState = (key: string, value: any) => {
-    sharedStateService.setState('dashboard', {
-      ...this.sharedState,
-      [key]: value,
-    });
-  };
-
   ngOnInit() {
-    this.sharedState = sharedStateService.getState('dashboard');
-    this.projects = this.sharedState.projects;
-    this.tasks = this.sharedState.tasks;
-    this.teams = this.sharedState.teams;
+    apiServiceInstance.getProjects().then((data: any) => {
+      this.projects = data;
+    });
+    apiServiceInstance.getTasks().then((data: any) => {
+      this.tasks = data;
+    });
+    apiServiceInstance.getTeam().then((data: any) => {
+      this.team = data;
+    });
   }
 }
